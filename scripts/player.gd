e77c4df47e91fbe08bcaed46cf031516a08372f7
kirjahri <150558369+kirjahri@users.noreply.github.com>
2025-07-18 15:37:43 -0400

    feat: add the ability to dive

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 
 @export_range(1, 10, 0.2) var gravity_multiplier: float = 2.0
+@export_range(1, 10, 0.2) var dive_multiplier: float = 2.0
 @export_range(-1000, 0, 0.2) var jump_velocity: float = -500.0
 
 
 func _physics_process(delta: float) -> void:
-	velocity += get_gravity() * gravity_multiplier * delta
+	if Input.is_action_pressed("dive"):
+		velocity += get_gravity() * gravity_multiplier * dive_multiplier * delta
+	else:
+		velocity += get_gravity() * gravity_multiplier * delta
 
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = jump_velocity
